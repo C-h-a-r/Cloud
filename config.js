8926a611a61c9dd63e75a4089fc446ace983da47
charweeeee <you@example.comecho>
2022-06-12 19:19:28 +0100

    updates

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  media: "OTIyOTQ2MTA0NTM2OTg5NzI2.GWogcT.h-f2ybyw5AGvRCsJCDR6fZ2x8UYIEDsQCBYGAo",
+  media: process.env.MEDIA,
   mediaID: "922946104536989726",
-  sheild: "NzgzNjg3NjA4NzEwMDcwMzMy.GuaK-U.1Ps0OP_7ETp0XXx-tAKZw0GBmvF87vJiMyl0gE",
+  sheild: process.env.SHEILD,
   sheildID: "783687608710070332",
-  mongodbUrl: "mongodb+srv://admin:FDPQnX6Ps7QyKbvy@database.7xozs11.mongodb.net/?retryWrites=true&w=majority"
+  mongodbUrl: process.env.MONGO
 }
